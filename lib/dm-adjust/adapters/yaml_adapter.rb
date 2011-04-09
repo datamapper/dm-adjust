9e8840be859ac621aa8d962ffb62586a1ae40176
Dan Kubb <dan.kubb@gmail.com>
2011-04-08 22:45:00 -0700

    Update code to match conventions in InMemoryAdapter extension
    * Conditionally add YamlAdapter#adjust if the adapter is being used

--- lib/dm-adjust/adapters/yaml_adapter.rb
+++ lib/dm-adjust/adapters/yaml_adapter.rb
@@ -1,26 +1,29 @@
 module DataMapper
   module Adapters
-    class YamlAdapter < AbstractAdapter
+    module YamlAdapterAdjust
       def adjust(attributes, collection)
-        query = collection.query
-
         attributes = attributes_as_fields(attributes)
+
         update_records(collection.model) do |records|
           records_to_update = collection.query.filter_records(records.dup)
           records_to_update.each do |record|
-            adjusted_attributes = adjust_attributes(attributes, record)
-            record.update(adjusted_attributes)
+            record.update(attributes) do |key, oldvalue, newvalue|
+              (oldvalue || 0) + newvalue
+            end
           end.size
         end
       end
+    end # module YamlAdapterAdjust
 
-      private
+    extendable do
+      # @api private
+      def const_added(const_name)
+        if const_name == :YamlAdapter
+          YamlAdapter.send(:include, YamlAdapterAdjust)
+        end
 
-      def adjust_attributes(attributes, record)
-        properties = record.reject { |key, val| !attributes.keys.include?(key) }
-        updated = attributes.merge(properties) { |key, old, new| old + new }
-        record.update(updated)
+        super
       end
     end
-  end
-end
+  end # module Adapters
+end # module DataMapper
